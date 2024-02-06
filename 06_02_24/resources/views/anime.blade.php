7edba8b7d80801f0c98e056b8110954332159587
Maicol Angelicchio <maicol.angelicchio.91@gmail.com>
2024-02-06 20:38:58 +0100

    migliorie varie

--- 06_02_24/resources/views/anime.blade.php
+++ 06_02_24/resources/views/anime.blade.php
@@ -1,32 +1,7 @@
 <x-main>
     <x-slot:title>Anime</x-slot>
+    <a href="{{ route('generi')}}">Generi</a>
     <h1>Anime</h1>
-    <style>
-        .container{
-            display: grid;
-            grid-template-columns: repeat(3, 1fr);
-            gap: 20px;
-        }
-
-        .card {
-            width: 300px;
-            border: 1px solid #ccc;
-            border-radius: 8px;
-            overflow: hidden;
-        }
-
-        .card-image {
-            width: 100%;
-            height: auto;
-        }
-
-        .card-title {
-            padding: 10px;
-            background-color: #f1f1f1;
-            font-size: 18px;
-            font-weight: bold;
-        }
-    </style>
         <div class="container">
             @foreach($listaAnime as $anime)
                 <div class="card">
--- 06_02_24/resources/views/components/main.blade.php
+++ 06_02_24/resources/views/components/main.blade.php
@@ -3,6 +3,32 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        .container{
+            display: grid;
+            grid-template-columns: repeat(3, 1fr);
+            gap: 20px;
+        }
+
+        .card {
+            width: 300px;
+            border: 1px solid #ccc;
+            border-radius: 8px;
+            overflow: hidden;
+        }
+
+        .card-image {
+            width: 100%;
+            height: auto;
+        }
+
+        .card-title {
+            padding: 10px;
+            background-color: #f1f1f1;
+            font-size: 18px;
+            font-weight: bold;
+        }
+    </style>
     <title>{{$title}}</title>
 </head>
 <body>
